15edad8edbdae399e10bea3de7fce1e52b900302
Darius <dariusford15@gmail.com>
2021-12-13 17:48:02 -0500

    part of the initial front-end design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={'grid grid-cols-12'}>
+
+        <div className={'col-span-12 bg-gray-200 h-16 p-3'}>
+            <h1 className={'text-xl font-bold text-center'}>Stock App</h1>
+        </div>
+
+        <div className={'col-span-7 bg-gray-300 h-96 p-3'}>
+
+          <div className={'flex'}>
+            <input type="search" 
+            className={'border w-1/2 border-gray-400 p-2 rounded-full'} 
+            placeholder="Search..."/>
+          
+            <button className={'bg-gray-500 text-white ml-12 px-4 rounded-md'}>Search</button>
+          </div>
+
+            <div className={'flex mt-8 ml-2 gap-24'}>
+              <h1 className={'text-3xl'}>BHP - BHP Group Limited</h1>
+              <button className={'bg-blue-500 px-4 rounded-md'}>Buy</button>
+              <button className={'bg-red-500 px-4 rounded-md'}>Sell</button>
+
+            </div>
+        </div>
+  
+        <div className={'col-span-5 bg-gray-400 h-96 p-3'}>Box 2</div>
+
+        </div>
     </div>
   );
 }
