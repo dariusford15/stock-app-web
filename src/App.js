87731c1dbdb8d9b80f07a9136b809d7e4645e9b3
Darius <dariusford15@gmail.com>
2022-01-13 22:12:49 -0500

    modification on web folders and files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 
-import './App.css';
 
-function App() {
+/*function App() {
+  const [searchSymbol, setSearchSymbol] = useState('');
+  const [stockName, setStockName] = useState();
+  const [stockPrice, setStockPrice] = useState();
+  const [buyValue, setBuyValue] = useState('');
+
+  const updateSearchSymbol = async (ev) => {
+    setSearchSymbol(ev.currentTarget.value)
+  };
+
+  const searchStock = async () =>{
+    console.log(' i was here')
+    console.log(`http://localhost:3005/api/search/${searchSymbol}`)
+    let res = await fetch(`http://localhost:3005/api/search/${searchSymbol}`)
+    let data = await res.json()
+    console.log(data)
+    // setStockName(searchSymbol)
+    // setStockPrice(res.data.price)
+    // setSearchSymbol('')
+    // console.log(data)
+
+  };
+  const buyStock = async ()=> {
+    let res = await fetch('http://localhost:3005/api/portfolio',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({buyValue, stockName, stockPrice})
+    })
+    return res.json();
+  };
+
   return (
     <div>
       <div className={'grid grid-cols-12'}>
@@ -14,25 +45,85 @@ function App() {
 
           <div className={'flex'}>
             <input type="search" 
+            value = {searchSymbol}
+            onChange = {updateSearchSymbol}
             className={'border w-1/2 border-gray-400 p-2 rounded-full'} 
             placeholder="Search..."/>
           
-            <button className={'bg-gray-500 text-white ml-12 px-4 rounded-md'}>Search</button>
+            <button onClick={searchStock} className={'bg-gray-500 text-white ml-12 px-4 rounded-md'}>Search</button>
           </div>
 
             <div className={'flex mt-8 ml-2 gap-24'}>
-              <h1 className={'text-3xl'}>BHP - BHP Group Limited</h1>
-              <button className={'bg-blue-500 px-4 rounded-md'}>Buy</button>
+              <h1 className={'text-3xl'}>{stockName}</h1>
+              <h2 className={'text-3xl'}>${stockPrice}</h2>
+
+              <input type = 'text' value= {buyValue} onChange = {(event)=> setBuyValue(event.currentTarget.value)} className={'border'}/>
+              <button onClick = {buyStock} className={'bg-blue-500 px-4 rounded-md'}>Buy</button>
               <button className={'bg-red-500 px-4 rounded-md'}>Sell</button>
 
             </div>
         </div>
   
-        <div className={'col-span-5 bg-gray-400 h-96 p-3'}>Box 2</div>
+        <div className={'col-span-5 bg-gray-400 h-96 p-3'}>
+          Portfolio
+          
+          <div className={'flex'}>
+          <table className={'table-fixed'}>
+          <thead>
+          <tr>
+            <th>Stock</th>
+            <th>Quantity</th>
+            <th>Value</th>
+            <th>Buy/Sell</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>BHP</td>
+            <td>50</td>
+            <td>2000.00</td>
+            <td className="text-center"><input type="checkbox"/></td>
+          </tr>
+          </tbody>
+
+        </table>
+          </div>
+        
+        </div>
+        
 
         </div>
     </div>
   );
 }
 
+export default App; */
+import './App.css';
+import Search from "./search";
+import Portfolio from "./portfolio";
+
+function App() {
+  return (
+    <>
+
+        <div className={'w-full p-5 bg-gray-300 border-b-2 border-gray-400'}>
+            <h1 className={'text-xl font-bold text-center text-gray-800 tracking-wider uppercase'}>Stock App</h1>
+
+        </div>
+
+        <div className="grid grid-cols-2">
+            <Search />
+            <Portfolio />
+
+        </div>
+
+
+    </>
+  );
+}
+
 export default App;
+
+
+
+
---
+++ src/search.js
@@ -0,0 +1,153 @@
+import React, {useState, useEffect} from 'react';
+// import yahooFinance from 'yahoo-finance';
+// import yahooStockPrices from 'yahoo-stock-prices'
+
+function Search(props) {
+    // yahooFinance.quote({
+    //     symbol: 'AAPL',
+    //     modules: [ 'price', 'summaryDetail' ] // see the docs for the full list
+    // }, function (err, quotes) {
+    //     console.log(err)
+    //     console.log(quotes)
+    // });
+    const [inputText, setInputText] = useState('');
+    const [currentStock, setCurrentStock] = useState();
+    const [ticker, setTicker] = useState();
+
+    const [buyQuantity, setBuyQuantity] = useState(0);
+
+    const [currentWallet, setCurrentWallet] = useState();
+
+    const fetchWallet = async () => {
+        const res = await fetch(`http://localhost:3005/api/wallet`);
+        let json = await res.json();
+        console.log(json)
+        setCurrentWallet(json)
+    };
+
+    useEffect(() => {
+        console.log('this runs only once on component load')
+
+
+        fetchWallet()
+    }, [])
+
+
+    const fetchQuote = async () => {
+        const res = await fetch(`http://localhost:3005/api/portfolio/search/${inputText}`);
+        let json = await res.json();
+        console.log(json)
+        setCurrentStock(json);
+        setTicker(inputText);
+        setInputText('')
+    };
+
+    const onInputChange = async (ev) => {
+        console.log(ev.currentTarget.value)
+        setInputText(ev.currentTarget.value);
+    };
+
+    // user clicks on the buy button
+    // on button click you trigger a function
+    // make a POST request to the API and update the portfolio
+
+
+    const buyStock = async () => {
+        console.log('buying the stock now')
+        console.log(currentWallet)
+
+        // check if the quantity is not 0
+        // check if the user has enough cash in teh wallet to be able to make a purchase
+        // if not show an error
+        // if yes, make a purchase and add a field in the portfolio table
+
+        if(buyQuantity === 0){
+            alert('Buy quantity needs to be greater than 0!')
+        }
+
+        let cashNeeded = buyQuantity * currentStock.data.price;
+        console.log('cashNeeded is', cashNeeded)
+
+        if(cashNeeded > currentWallet.value){
+            alert('You dont have enough cash!');
+        } else {
+            // make the purchase
+            // making a field in the database table for portfolio
+            let body = {
+                symbol: ticker,
+                quantity: buyQuantity,
+                price: currentStock.data.price,
+
+            }
+
+            let options = {
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: {}
+
+            };
+
+            options.headers["Accept"] = "application/json, text/plain, */*";
+            options.headers["Content-Type"] = "application/json;charset=utf-8";
+
+            console.log(options);
+
+
+            const res = await fetch(`http://localhost:3005/api/portfolio`, options);
+            let json = await res.json();
+            console.log(json)
+
+            setBuyQuantity(0)
+
+            alert('Success!')
+
+            window.location.reload();
+
+
+
+
+        }
+
+        // stock symbol
+        // stock quote
+    };
+
+    const onBuyChange = async (ev) => {
+        setBuyQuantity(ev.currentTarget.value);
+    };
+
+    return (
+        <div className={'border p-5'}>
+
+            <div className="grid grid-cols-2">
+                <div className="flex flex-row justify-start">
+                    <div className="flex-grow flex-shrink w-30 items-center">
+                        <input value={inputText} onChange={onInputChange} type="text" className={'border w-30 border-gray-400 p-2 rounded-full'} placeholder="Search.."/>
+                    </div>
+                    <div className="flex-grow flex-shrink w-40 items-center">
+                    <button onClick={fetchQuote} className={'bg-gray-500 text-white ml-14 py-2 rounded-md cursor-pointer w-40'}>Search</button>
+                    </div>
+                </div>
+            </div>
+
+            {ticker && <div className="grid grid-cols-2">
+                <div className={'border p-5'}>
+                    {/*{currentStock && <h1 className={'text-2xl'}>{currentStock.data}</h1>}*/}
+                    <h1 className={'text-2xl'}>{ticker} : {currentStock && <span>&nbsp;&nbsp;{currentStock.data.currency} {currentStock.data.price}</span>}</h1>
+                </div>
+                <div className={'border p-5'}>
+
+                    <span>
+                        <input type="number" onChange={onBuyChange} className={'border'} value={buyQuantity} />&nbsp;&nbsp;
+                        <span className={'bg-blue-600 cursor-pointer p-2 rounded text-white text-xl pl-5 pr-5'} onClick={buyStock}>Buy</span>&nbsp;&nbsp;&nbsp;
+                        </span>
+                    {/*<span className={'bg-red-600 cursor-pointer p-2 rounded text-white text-xl pl-5 pr-5'}>Sell</span>*/}
+
+                </div>
+            </div>}
+
+        </div>
+    );
+}
+
+export default Search;
